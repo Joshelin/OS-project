319dd210f8de439b09c9eec78c69592961ab9d62
Plutone11011 <lorenzo97@fastwebnet.it>
2018-01-03 16:55:01 +0100

    corretto indirizzo

--- PCBAlloc/Alloc.c
+++ PCBAlloc/Alloc.c
@@ -6,7 +6,7 @@ pcb_t *temp = pcbfree_h;
 Scorro lista pcbfree_h tramite temp.*/
 void initPcbs() {
 	if (i < MAXPROC) {  
-		temp = pcbFree_table[i];
+		temp = &pcbFree_table[i];
 		temp = temp->p_next;
 		i++;
 		initPcbs();
