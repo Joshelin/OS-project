3a18f37e7046351e5f8e201a931f599e89759fb9
Federico <xmayax96@gmail.com>
2018-01-04 19:10:42 +0100

    Sistemato initPcbs

--- PCBAlloc/Alloc.c
+++ PCBAlloc/Alloc.c
@@ -5,8 +5,15 @@ pcb_t *temp = pcbfree_h;
 /* Ricorsivo v1, da controllare.
 Scorro lista pcbfree_h tramite temp.*/
 void initPcbs() {
-	if (i < MAXPROC) {  
+	// Solo per lista ancora vuota.
+	if (i == 0){
 		temp = &pcbFree_table[i];
+		temp->p_next = &pcbFree_table[i+1];
+		temp = temp->p_next;
+		i += 2;
+	}
+	if (i < MAXPROC) {  
+		temp->p_next = &pcbFree_table[i];
 		temp = temp->p_next;
 		i++;
 		initPcbs();
