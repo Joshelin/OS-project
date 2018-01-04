ce1babad1bf5efd52a1d4c8038e299619e9e2e36
Federico <xmayax96@gmail.com>
2018-01-05 00:20:35 +0100

    Ottimizzato e corretto momentaneamente errori di compilazione.

--- PCBAlloc/Alloc.c
+++ PCBAlloc/Alloc.c
@@ -1,21 +1,19 @@
 #include "Alloc.h"
 // variabili globali qui
 int i = 0;
-pcb_t *temp = pcbfree_h;
-/* Ricorsivo v1, da controllare.
-Scorro lista pcbfree_h tramite temp.*/
+pcb_t *temp = (pcb_t *)&pcbfree_h; // Così piace a gcc, non sono sicuro al 100% del motivo, ma senza il cast a puntatore pcb_t non gli piaceva.
+
 void initPcbs() {
 	// Solo per lista ancora vuota.
 	if (i == 0){
-		temp = &pcbFree_table[i];
-		temp->p_next = &pcbFree_table[i+1];
+		temp = &pcbFree_table[i++];
+		temp->p_next = &pcbFree_table[i];
 		temp = temp->p_next;
 		i += 2;
 	}
 	if (i < MAXPROC) {  
-		temp->p_next = &pcbFree_table[i];
+		temp->p_next = &pcbFree_table[i++];
 		temp = temp->p_next;
-		i++;
 		initPcbs();
 	}
 }
