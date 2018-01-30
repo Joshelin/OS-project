fc1460b74a531bf5db7fdb8e0456cec28e2c2d94
Federico <xmayax96@gmail.com>
2018-01-30 11:11:14 +0100

    Queue.c modificato

--- PCBQueue/Queue.c
+++ PCBQueue/Queue.c
@@ -3,8 +3,8 @@
 void insertProcQ(pcb_t **head, pcb_t *p){
 	if (*head == NULL) {
 		*head = p;
-	} else if (*head->priority <= p->priority){
+	} else if ((*head)->priority <= p->priority){
 		p->p_next = *head;
-		*head = p;
+		(*head) = p;
 	}
 }
